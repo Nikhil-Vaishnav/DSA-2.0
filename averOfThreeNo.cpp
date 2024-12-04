fdf4228ed4dc712bbd2d3793d5e049ba5c56fbef
Nikhil Vaishnav <nikhilvaishnav364@gmail.com>
2024-12-04 22:49:00 +0530

    new solution added

---
+++ averOfThreeNo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a , b , c , sum;
+
+    cout<<"Enter the first no. :- ";
+    cin>>a;
+
+    cout<<"Enter the Second no. :- ";
+    cin>>b;
+
+    cout<<"Enter the Third no. :- ";
+    cin>>c;
+
+    sum = (a + b + c)/3;
+  
+    cout<<"Sum of Three no. is :- "<<sum;
+
+    return 0;
+}
